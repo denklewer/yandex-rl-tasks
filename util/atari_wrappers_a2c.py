17e8b0afe3aeaec54e14324f13040ae795f0ea7e
denklewer <denklewer@gmail.com>
2019-04-18 17:51:45 +0300

    debug printing

--- util/atari_wrappers_a2c.py
+++ util/atari_wrappers_a2c.py
@@ -151,6 +151,8 @@ class QueueFrames(gym.ObservationWrapper):
       oshape = ospace.shape + (nframes,)
     self.observation_space = spaces.Box(ospace.low.min(), ospace.high.max(),
                                         oshape, ospace.dtype)
+    print("observations_space size will be ", self.observation_space)
+
 
   def observation(self, observation):
     self.obs_queue.append(observation)
